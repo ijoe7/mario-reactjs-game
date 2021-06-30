9da7f500402a3ae886a06f7a57aaf961b40093e7
Iyanda Oluwasegun <iyandaoluwasegun7@gmail.com>
2021-06-30 19:58:52 +0100

    correction

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -3,9 +3,7 @@ export default function Welcome(props) {
     <div>
       <h2>React Maze Game</h2>
       <p>
-        You need to eat all the mashrooms in 64 steps. By Default maze size is
-        10 x 10, you can change it by reloading or entering the height and width
-        in prompt.
+        Mario needs to catch all the Toads in 60 steps.
       </p>
     </div>
   );
